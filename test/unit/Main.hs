004f03aec46f29069fd0bf5115a384cd2b3141b3
Tommy Bidne <tbidne@protonmail.com>
2022-12-07 13:13:25 +1300

    Remove unnecessary MaxRuns type

--- test/unit/Main.hs
+++ test/unit/Main.hs
@@ -1,16 +1,11 @@
 module Main (main) where
 
-import Data.Data (Proxy (..))
 import Props.Data.Bounds (props)
-import Props.MaxRuns (MaxRuns)
-import Test.Tasty
-import Test.Tasty.Options (OptionDescription (..))
+import Test.Tasty (defaultMain, testGroup)
 
 main :: IO ()
-main = do
-  let options = includingOptions [Option @MaxRuns Proxy]
-      ingredients = options : defaultIngredients
-  defaultMainWithIngredients ingredients $
+main =
+  defaultMain $
     testGroup
       "Property tests"
       [ props
--- test/unit/Props/Data/Bounds.hs
+++ test/unit/Props/Data/Bounds.hs
@@ -18,12 +18,10 @@ import Hedgehog
     assert,
     forAll,
     property,
-    withTests,
     (===),
   )
 import Props.Generators (BoundedType (..), genBounded)
-import Props.MaxRuns (MaxRuns (..))
-import Test.Tasty (TestName, TestTree, askOption, testGroup)
+import Test.Tasty (TestName, TestTree, testGroup)
 #if MIN_VERSION_tasty_hedgehog(1, 2, 0)
 import Test.Tasty.Hedgehog (testPropertyNamed)
 #else
@@ -45,41 +43,39 @@ props =
 --
 -- @since 0.1
 maxBoundSynced :: TestTree
-maxBoundSynced = askOption $ \(MkMaxRuns limit) ->
+maxBoundSynced =
   testPropertyCompat "maxBound === upperBound" "maxBoundSynced" $
-    withTests limit $
-      property $
-        forAll genBounded >>= \case
-          MkBoundedType p@(_ :: Proxy a) -> do
-            annotate $ showTy p
-            annotateShow $ maxBound @a
-            annotateShow $ upperBound @a
-            maxBound @a === upperBound
-          MkBoundedTypeNoEq p@(_ :: Proxy a) eqFn -> do
-            annotate $ showTy p
-            annotateShow $ maxBound @a
-            annotateShow $ upperBound @a
-            assert $ maxBound @a `eqFn` upperBound
+    property $
+      forAll genBounded >>= \case
+        MkBoundedType p@(_ :: Proxy a) -> do
+          annotate $ showTy p
+          annotateShow $ maxBound @a
+          annotateShow $ upperBound @a
+          maxBound @a === upperBound
+        MkBoundedTypeNoEq p@(_ :: Proxy a) eqFn -> do
+          annotate $ showTy p
+          annotateShow $ maxBound @a
+          annotateShow $ upperBound @a
+          assert $ maxBound @a `eqFn` upperBound
 
 -- | minBound === lowerBound
 --
 -- @since 0.1
 minBoundSynced :: TestTree
-minBoundSynced = askOption $ \(MkMaxRuns limit) ->
+minBoundSynced =
   testPropertyCompat "minBound === lowerBound" "minBoundSynced" $
-    withTests limit $
-      property $
-        forAll genBounded >>= \case
-          MkBoundedType p@(_ :: Proxy a) -> do
-            annotate $ showTy p
-            annotateShow $ minBound @a
-            annotateShow $ lowerBound @a
-            minBound @a === lowerBound
-          MkBoundedTypeNoEq p@(_ :: Proxy a) eqFn -> do
-            annotate $ showTy p
-            annotateShow $ minBound @a
-            annotateShow $ lowerBound @a
-            assert $ minBound @a `eqFn` lowerBound
+    property $
+      forAll genBounded >>= \case
+        MkBoundedType p@(_ :: Proxy a) -> do
+          annotate $ showTy p
+          annotateShow $ minBound @a
+          annotateShow $ lowerBound @a
+          minBound @a === lowerBound
+        MkBoundedTypeNoEq p@(_ :: Proxy a) eqFn -> do
+          annotate $ showTy p
+          annotateShow $ minBound @a
+          annotateShow $ lowerBound @a
+          assert $ minBound @a `eqFn` lowerBound
 
 showTy :: Typeable a => Proxy a -> String
 showTy = show . typeOf
