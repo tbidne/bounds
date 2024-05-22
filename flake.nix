f93b202c9d154b25b42b048570cf6648451aa577
Tommy Bidne <tbidne@protonmail.com>
2024-05-22 23:03:44 +1200

    Move default-language, formatting

--- flake.nix
+++ flake.nix
@@ -4,24 +4,29 @@
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   inputs.nix-hs-utils.url = "github:tbidne/nix-hs-utils";
   outputs =
-    inputs@{ flake-parts
-    , nix-hs-utils
-    , nixpkgs
-    , self
+    inputs@{
+      flake-parts,
+      nix-hs-utils,
+      nixpkgs,
+      self,
     }:
     flake-parts.lib.mkFlake { inherit inputs; } {
-      perSystem = { pkgs, ... }:
+      perSystem =
+        { pkgs, ... }:
         let
           ghc-version = "ghc982";
           hlib = pkgs.haskell.lib;
           compiler = pkgs.haskell.packages."${ghc-version}";
-          mkPkg = returnShellEnv:
+          mkPkg =
+            returnShellEnv:
             nix-hs-utils.mkHaskellPkg {
               inherit compiler pkgs returnShellEnv;
               name = "bounds";
               root = ./.;
             };
-          compilerPkgs = { inherit pkgs compiler; };
+          compilerPkgs = {
+            inherit pkgs compiler;
+          };
         in
         {
           packages.default = mkPkg false;
